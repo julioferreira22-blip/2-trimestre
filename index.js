c7d55d22311ab12da3ce9d35d6b3d23ea746f598
Júlio <julio.ferreira22@escola.pr.gov.br>
2026-06-15 08:45:32 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -5,7 +5,28 @@ const express = require("express")
 const app = express()
 const port = 3000
 app.use(express.json())
+const fs = require('fs')
 
+app.post("/clientes", (req, res) =>{
+    const cliente = req.body
+   
+try{
+    
+    //abrir o arquivbo
+    const bd = JSON.parse(fs.readFileSync("bd.json", "utf8"))
+     //adicionar cliente
+      bd.push(cliente)
+      //salvar o arquivo 
+fs.writeFileSync("bd.json", JSON.stringify(bd), "utf8")
+    //resposta
+    res.status(201).json({resposta: "Cliente cadastrado!"}) 
+
+}catch (erro) {
+    res.status(500).json({erro:erro.message})
+}
+  
+   
+})
 
 app.get('/ola', (req, res) => {
     res.send({resposta: "olá mundo!"})
